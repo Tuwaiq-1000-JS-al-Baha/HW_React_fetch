023ca76d9bd4020bf4ceaba5dbb2775588230dd5
Ghadah-Mohammed <92169172+Ghadah-Mohammed@users.noreply.github.com>
2021-11-12 13:53:27 +0300

    Hw

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+// import NewItem from "./component/NewItem"
+import NewsGroup from "./Component/NewsGroup"
 function App() {
-  return <></>
-}
+  const [News, setNews] = useState([])
+  useEffect(() => {
+    axios
+      .get(
+        "https://newsapi.org/v2/everything?q=tesla&from=2021-10-12&sortBy=publishedAt&apiKey=0897dcbc10bd4582b3c3b78142f0ba8e"
+      )
+      .then(response => {
+        setNews(response.data.articles)
+      })
+  }, [])
 
+  return (
+    <>
+      {News.map(New => (
+        <NewsGroup New={New} />
+      ))}
+    </>
+  )
+}
 export default App
