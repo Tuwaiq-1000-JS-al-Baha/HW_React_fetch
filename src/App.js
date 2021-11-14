93b1727a8b248f77d6e88f37cd5a921ee3163b7f
Abdulathim96 <Abdulathim.Abdullah.abt.js@tuwaiq.edu.sa>
2021-11-14 10:21:48 +0300

    HW done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-function App() {
-  return <></>
+import axios from "axios"
+import Cards from "./components/Cards"
+import { useEffect, useState } from "react"
+import "./App.css"
+
+function News() {
+  const [News, setNews] = useState([])
+  useEffect(() => {
+    axios
+      .get(
+        "https://newsapi.org/v2/everything?q=apple&from=2021-11-09&to=2021-11-09&sortBy=popularity&apiKey=fde3ae16be4340ffa648d53190b865f9"
+      )
+      .then(response => {
+        setNews(response.data.articles)
+      })
+  }, [])
+
+  return (
+    <>
+      {News.map(New => (
+        <Cards New={New} />
+      ))}
+    </>
+  )
 }
 
-export default App
+export default News
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
-
+import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 
-ReactDOM.render(<App />, document.querySelector("#root"))
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+)
